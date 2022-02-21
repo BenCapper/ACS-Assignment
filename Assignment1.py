3065240e2d8e4ced67b1148171ed8c013ae0f7c4
Ben Capper <Bencapper19@gmail.com>
2022-02-21 21:37:07 +0000

    add todo comments
    Signed-off-by: Ben Capper <Bencapper19@gmail.com>

--- Assignment1.py
+++ Assignment1.py
@@ -10,10 +10,29 @@ client = boto3.client("ec2")
 # print(response)
 
 user_data = (
-    "#!/bin/bash \nyum -y install httpd \nsystemctl enable httpd \nservice httpd start"
+    """
+#!/bin/bash
+yum update -y
+yum install httpd -y
+systemctl enable httpd
+systemctl start httpd"""
 )
 
+
+# Check if the keypair already exists
+key_response = client.create_key_pair(
+    KeyName = 'assign_one',
+    KeyType = 'rsa'
+)
+assign_one_key = key_response["KeyMaterial"]
+key_name = key_response["KeyName"]
+key_id = key_response["KeyPairId"]
+# Save key to file
+# Change permissions on file
+
+
 try:
+    # Check if security group already exists
     response = client.describe_security_groups(GroupNames=["Assignment"])
     grp_id = list()
     grp_id.append(response["SecurityGroups"][0]["GroupId"])
@@ -21,13 +40,18 @@ try:
 except ClientError as e:
     print(e)
 
+
 resource.create_instances(
-    ImageId="ami-0bf84c42e04519c85",
-    KeyName = "benkey",
-    UserData=user_data,
-    InstanceType="t2.micro",
-    SecurityGroupIds=grp_id,
-    MinCount=1,
-    MaxCount=1,
+    ImageId = "ami-0bf84c42e04519c85",
+    KeyName = key_name,
+    UserData = user_data,
+    InstanceType = "t2.micro",
+    SecurityGroupIds = grp_id,
+    MinCount = 1,
+    MaxCount = 1,
 )
+# Wait until finish
+# SSH in and get metadata
+# Configure index and scp image and text
+# Print metadata
 
