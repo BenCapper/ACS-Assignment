d75e30f6c59568dc1efd6d9937090115972ff670
Ben Capper <Bencapper19@gmail.com>
2022-02-23 19:52:19 +0000

    ec2 steps
    Signed-off-by: Ben Capper <Bencapper19@gmail.com>

--- Assignment1.py
+++ Assignment1.py
@@ -1,11 +1,16 @@
-from asyncio import subprocess
+import subprocess
 import os
+import time
 import boto3
+import requests
 from botocore.exceptions import ClientError
 
 resource = boto3.resource("ec2")
 client = boto3.client("ec2")
 
+key_name = ""
+key_response = ""
+key_name_list = ["assign_one"]
 
 # response = client.describe_images(Filters=[{'Name': 'image-id', 'Values': ['ami-0bf84*']}])
 # print(response)
@@ -20,28 +25,44 @@ systemctl start httpd
 echo '<html>' > index.html
 echo 'Private IP address: ' >> index.html
 curl http://169.254.169.254/latest/meta-data/local-ipv4 >> index.html
+echo '<br>' >> index.html
+echo 'Availability Zone: ' >> index.html
+curl http://169.254.169.254/latest/meta-data/placement/availability-zone >> index.html
+echo '<br>' >> index.html
+echo 'Subnet: ' >> index.html
+MAC=$(curl http://169.254.169.254/latest/meta-data/mac)
+curl http://169.254.169.254/latest/meta-data/network/interfaces/macs/${MAC}/subnet-id >> index.html
 cp index.html /var/www/html/index.html"""
 )
 
+try:
+    found_key_name = client.describe_key_pairs(KeyNames=key_name_list)["KeyPairs"][0]["KeyName"]
+except:
+    found_key_name = ""    
 
-# Check if the keypair already exists
-key_response = client.create_key_pair(
-    KeyName = 'assign_one',
-    KeyType = 'rsa'
-)
-assign_one_key = key_response["KeyMaterial"]
-key_name = key_response["KeyName"]
-key_id = key_response["KeyPairId"]
+if key_name_list[0] != found_key_name:
+    key_response = client.create_key_pair(
+        KeyName = 'assign_one',
+        KeyType = 'rsa'
+    )
+    assign_one_key = key_response["KeyMaterial"]
+    key_name = key_response["KeyName"]
+    key_id = key_response["KeyPairId"]
+    with open('assign_one.pem', 'w') as f:
+        f.write(assign_one_key)
+else:
+    key_response = client.describe_key_pairs(KeyNames=key_name_list)["KeyPairs"][0]
+    key_name = key_response["KeyName"]
+    key_id = key_response["KeyPairId"]
+
+
 # Save key to file
 # Change permissions on file
-
-
 try:
     # Check if security group already exists
-    response = client.describe_security_groups(GroupNames=["Assignment"])
+    response = client.describe_security_groups(GroupNames=["ssh-http"])
     grp_id = list()
     grp_id.append(response["SecurityGroups"][0]["GroupId"])
-    print(id)
 except ClientError as e:
     print(e)
 
@@ -56,16 +77,18 @@ create_response = resource.create_instances(
     MaxCount = 1,
 )
 
-# Check response for the instance id
-print(create_response)
-create_response.wait_until_running()
+
+created_instance = create_response[0]
+created_instance.wait_until_running()
 print("Instance Running")
-create_response.reload()
-# Get the instance IP address
-ip_addr = 0
-ssh_command = "ssh -o StrictHostKeyChecking=no -i ${key_name}.pem ec2-user@${ip_addr}"
+time.sleep(20)
+created_instance.reload()
+created_instance.wait_until_running()
+public_ip = created_instance.public_ip_address
+subprocess.run("chmod 400 assign_one.pem", shell=True)
+ssh_command = f"ssh -o StrictHostKeyChecking=no -i {key_name}.pem ec2-user@{public_ip} 'echo Public IPV4: {public_ip}'"
 subprocess.run(ssh_command, shell=True)
-# Wait until finish
+
 # SSH in and get metadata
 # Configure index and scp image and text
 # Print metadata
